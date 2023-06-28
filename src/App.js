ecf4fec7dd17ad70a08f7df9fd7fe29de92cda33
vladislavponomaryov96 <vladislavponomaryov96@gmail.com>
2023-06-28 17:29:02 +0600

    stage 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Layout from './components/layout'
+
 function App() {
-  return (
-    <h1>My app</h1>
-  );
+	return (
+		<h1>
+			My app
+			<Layout />
+		</h1>
+	)
 }
 
-export default App;
+export default App
